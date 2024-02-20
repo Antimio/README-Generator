6064aa52f6c239dca752a8081c3e79f840308152
Antimio <antimiocasimiro@gmail.com>
2024-02-20 19:09:04 +0000

    Completed the generateMarkdown function with the actual scaffolding of the readme file as a string literal that will take object values and produce the readme to the especifications of the user

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# caidv
+  
+## Description
+  
+vsdv ac
+  
+## Table of Contents
+  
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#Uicense)
+- [Contributing](#Contributing)
+- [Tests](#App_tests)
+- [Questions](#Questions)
+  
+## Installation
+  
+cakdvcae
+  
+## Usage
+  
+c lvdsavuad
+  
+## License
+  
+This project is licensed under the MIT Licence license.
+  
+## Contributing
+  
+caovcad
+  
+## Tests
+  
+undefined
+  
+## Questions
+  
+For any questions, please contact me at lbvewvb or visit my GitHub profile https://github.com/cvlobeyvleq.
+
+
+  
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,47 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.Title}
-`
+
+    return `
+# ${data.Title}
+  
+## Description
+  
+${data.Description}
+  
+## Table of Contents
+  
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#Uicense)
+- [Contributing](#Contributing)
+- [Tests](#App_tests)
+- [Questions](#Questions)
+  
+## Installation
+  
+${data.Installation}
+  
+## Usage
+  
+${data.Usage}
+  
+## License
+  
+This project is licensed under the ${data.License} license.
+  
+## Contributing
+  
+${data.Contributing}
+  
+## Tests
+  
+${data.Tests}
+  
+## Questions
+  
+For any questions, please contact me at ${data.Email_address} or visit my GitHub profile https://github.com/${data.Github_name}.
+
+   `;
 }
 
 module.exports = generateMarkdown;
